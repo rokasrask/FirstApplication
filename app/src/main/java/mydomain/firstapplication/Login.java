12b80eea02776491ac58bba9ca28858a0a414f28
moksleivis <e.boruze@gmail.com>
2018-02-13 10:15:37 +0200

    test

--- app/src/main/java/mydomain/firstapplication/Login.java
+++ app/src/main/java/mydomain/firstapplication/Login.java
@@ -1,6 +1,7 @@
 package mydomain.firstapplication;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -11,6 +12,7 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.Toast;
+import android.database.Cursor;
 
 import java.lang.reflect.Array;
 
@@ -28,7 +30,6 @@ public class Login extends AppCompatActivity {
         /*ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.)*/
         final DBcats dbHandler = new DBcats(this, null, null, 1);
-        final String catstuff = "";
         final Button buttonback2 = (Button) findViewById(R.id.button4);
         final Button buttonadd = (Button) findViewById(R.id.buttonadd);
         final Button buttonDisplay = (Button) findViewById(R.id.displayButton);
@@ -53,46 +54,91 @@ public class Login extends AppCompatActivity {
 
 
         buttonadd.setOnClickListener(new View.OnClickListener() {
+           
             @Override
             public void onClick(View v) {
 
+
+
+
+                String catstuff = "";
+                if (cb1.isChecked()) {
+                    catstuff +="Interesting, ";
+                }
+                if (cb2.isChecked()) {
+                    catstuff += "Very, ";
+                }
+                if (cb3.isChecked()) {
+                    catstuff += "Yes. ";
+                }
                 int selectedRb = radiogroup.getCheckedRadioButtonId();
                 RadioButton radioButton = (RadioButton) findViewById(selectedRb);
 
-                Toast.makeText(Login.this,
-                        "Id: " + etId.getText().toString() + "\n" +
-                                "Name: " + etName.getText().toString() + "\n" +
-                                "Facts: " + etFacts.getText().toString() + "\n" +
-                                "Words: " + etWords.getText().toString() + "\n" +
-                                "Interesting :" + catstuff.toString() + "\n" +
-                                radioButton.getText(), Toast.LENGTH_SHORT).show();
+                if(!Validation.isValidId(etId.getText().toString())) {
+                    etId.setError("Enter a valid ID!");
+                }
+                else if(!Validation.isValidCredentials(etName.getText().toString())) {
+                    etName.setError("Enter a valid name");
+                }
 
-                Facts facts = new Facts(etName.getText().toString(), etWords.getText().toString(), etFacts.getText().toString(),  catstuff.toString(), (String) radioButton.getText());
-                dbHandler.addCatfact(facts);
+               else {
+                    Toast.makeText(Login.this,
+                                    "Id: " + etId.getText().toString() + "\n" +
+                                    "Name: " + etName.getText().toString() + "\n" +
+                                    "Facts: " + etFacts.getText().toString() + "\n" +
+                                    "Words: " + etWords.getText().toString() + "\n" +
+                                    "Interesting :" + catstuff  + "\n" +
+                                    radioButton.getText(), Toast.LENGTH_SHORT).show();
+
+                    Facts facts = new Facts(etName.getText().toString(), etWords.getText().toString(), etFacts.getText().toString(),catstuff , radioButton.getText().toString());
+                    dbHandler.addCatfact(facts);
+                }
             }
+
+
+
+
+
+
         });
 
         buttonDisplay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+                Cursor res = dbHandler.getAllData();
+                if (res.getCount() == 0) {
+                    showMessage("Error", "No data available");
+                    return;
+                } else {
+                    StringBuffer buffer = new StringBuffer();
+                    while (res.moveToNext()) {
+                        buffer.append("id: " + res.getString(0) + "\n");
+                        buffer.append("name: " + res.getString(1) + "\n");
+                        buffer.append("words: " + res.getString(2) + "\n");
+                        buffer.append("fact: " + res.getString(3) + "\n");
+                        buffer.append("interesting: " + res.getString(4) + "\n");
+                        buffer.append("type: " + res.getString(5) + "\n");
+                        buffer.append("cats: " + res.getString(6) + "\n");
+                    }
+
+                    showMessage("Data", buffer.toString());
+
+
+                }
             }
         });
 
     }
 
-    public void isChecked(View view, CheckBox cb1, CheckBox cb2, CheckBox cb3, String catstuff) {
-        if (cb1.isChecked()) {
-            catstuff +="Interesting, ";
-        }
-        if (cb2.isChecked()) {
-            catstuff += "Very, ";
-        }
-        if (cb3.isChecked()) {
-            catstuff += "Yes. ";
-        }
-
+    public void showMessage(String title, String Message){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(Message);
+        builder.show();
     }
+
+
 
 
 
